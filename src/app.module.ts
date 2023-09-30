7af3b810efde754e0ba965e672ea0143017f633c
edcastillob <edwar.castillo@gmail.com>
2023-09-29 23:27:23 -0400

    Instalación de la dependencia de configuración

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GitDataUserModule } from './git-data-user/git-data-user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GitDataUserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:`.${process.env.NODE_ENV.trim()}.env`,
+      isGlobal:true
+    }),
+    GitDataUserModule
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constant';
 
-async function server() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.use(morgan('dev'));
+  app.enableCors(CORS)
+
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port);
+  console.log(`⚡📱 Application running on port ${port} ...`);
 }
-server();
+bootstrap();
