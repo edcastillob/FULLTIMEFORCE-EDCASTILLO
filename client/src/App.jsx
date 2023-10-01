895d72e58e1616a08f47cba6fa7b2cce2d4d6635
edcastillob <edwar.castillo@gmail.com>
2023-10-01 04:00:19 -0400

    - Rutas y carga del componente Landing y footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Commits } from './components/commits/Commits'
 import { MyNavbar } from './components/navbar/MyNavbar'
 import { Home } from './components/home/Home';
+import { Footer } from './components/footer/Footer';
+import { Landing } from './landing/Landing';
 
 
 function App() {
@@ -10,15 +12,14 @@ function App() {
   return (
     
       <div>
-        {location.pathname !== "/" && <MyNavbar />}     
-       
+        {location.pathname !== "/" && <MyNavbar />}           
         <Routes>
+        <Route exact path="/" element={<Landing />} />
         <Route exact path="/" element={<Home />} />
         <Route exact path="/home" element={<Home />} />
-        <Route exact path="/commits" element={<Commits />} />
-        {/* <Route exact path="/docs" element={<Result />} /> */}
-        {/* <Route path="/commits/:id" element={<EditSurvey />} /> */}
+        <Route exact path="/commits" element={<Commits />} />        
       </Routes>
+      <Footer />
       </div>
      
     
